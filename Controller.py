6f3210b1a31285913ecf0c20f53f333eaf0a8b09
Kate McCurley <cmccurley@olin.edu>
2023-02-13 01:40:43 -0500

    update controller class

--- Controller.py
+++ Controller.py
@@ -18,18 +18,11 @@ class Controller3D():
 
         self.params = cfparams
         self.pid_gains = pid_gains
+
+        self.last_angle_errors = [0, 0, 0]
+        self.e_trans_sum = [0, 0, 0]
+        self.e_rot_sum = [0, 0, 0]
         
-        self.se_x = 0
-        self.se_y = 0
-        self.se_z = 0
-        
-        self.se_phi = 0
-        self.se_theta = 0
-        self.se_psi = 0
-        
-        self.U = np.array([0.,0.,0.,0.])
-        self.a_x = 0
-        self.a_y = 0
 
 
     def compute_commands(self, setpoint, state, dt, calc_z=True, calc_xy=True, calc_ptp=True):
@@ -43,10 +36,77 @@ class Controller3D():
         N.B. TrajPoint is a new dataclass. Please check it out from the utils.py script
         """
 
+        U = np.array([0.,0.,0.,0.])
+
+        # TRANSLATIONAL 
+
+        # position error
+        e_x_pos = setpoint.x_pos - state.x_pos
+        e_y_pos = setpoint.y_pos - state.y_pos
+        e_z_pos = setpoint.z_pos - state.z_pos
+
+        self.e_trans_sum = [sum(x) for x in zip(self.e_trans_sum, \
+            [e_x_pos, e_y_pos, e_z_pos])]
+
+        # velocity error
+        e_x_vel = setpoint.x_vel - state.x_vel
+        e_y_vel = setpoint.y_vel - state.y_vel
+        e_z_vel = setpoint.z_vel - state.z_vel
+
+        # calculated acceleration
+        a_x = self.pid_gains["kp_x"] * e_x_pos + \
+            self.pid_gains["kd_x"] * e_x_vel + \
+            self.pid_gains["ki_x"] * self.e_trans_sum[0]
+        a_y = self.pid_gains["kp_y"] * e_y_pos + \
+            self.pid_gains["kd_y"] * e_y_vel + \
+            self.pid_gains["ki_y"] * self.e_trans_sum[1]
+        a_z = self.pid_gains["kp_z"] * e_z_pos + \
+            self.pid_gains["kd_z"] * e_z_vel + \
+            self.pid_gains["ki_z"] * self.e_trans_sum[2]
+        
+        # angular position error
+        phi_d = (1 / self.params.g) * \
+            (a_x * sin(state.psi) - a_y * cos(state.psi))
+        theta_d = (1 / self.params.g) * \
+            (a_x * cos(state.psi) + a_y * sin(state.psi))
+        psi_d = state.psi
+
+        e_phi = phi_d - state.phi
+        e_theta = theta_d - state.theta
+        e_psi = psi_d - state.psi
+
+        self.e_rot_sum = [sum(x) for x in zip(self.e_rot_sum, \
+            [e_phi, e_theta, e_psi])]
+
+        # angular velocity error 
+        e_p = (e_phi - self.last_angle_errors[0]) / dt
+        e_q = (e_theta - self.last_angle_errors[1]) / dt
+        e_r = (e_psi - self.last_angle_errors[2]) / dt
+
+        # PID control equations
+        U[0] = self.params.mass * (a_z + self.params.g)
+        U[1] = self.pid_gains["kd_p"] * e_p + \
+            self.pid_gains["kp_phi"] * e_phi + \
+            self.pid_gains["ki_phi"] * self.e_rot_sum[0]
+        U[2] = self.pid_gains["kd_q"] * e_q + \
+            self.pid_gains["kp_theta"] * e_theta + \
+            self.pid_gains["ki_theta"] * self.e_rot_sum[1]
+        U[3] = self.pid_gains["kd_r"] * e_r + \
+            self.pid_gains["kp_psi"] * e_psi + \
+            self.pid_gains["ki_psi"] * self.e_rot_sum[2]
+
+        # save angle errors
+        self.last_angle_errors = [e_phi, e_theta, e_psi]
+        
+        return U
+
+
+
         # translational
 
+
         # U1: z
-        if calc_z:
+        '''if calc_z:
             e_pos_z = setpoint.z_pos - state.z_pos
             e_vel_z = setpoint.z_vel - state.z_vel
             self.se_z += e_pos_z*dt
@@ -157,4 +217,4 @@ class Controller3D():
 
     #     self.last_psi = state.psi
         
-    #     return self.U
+    #     return self.U'''
